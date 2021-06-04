a72c5e58e80052788987db2c72d333dc0c00736e
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2021-06-04 13:23:45 +0300

    Start state

--- material-part-1/src/App.js
+++ material-part-1/src/App.js
@@ -1,28 +1,42 @@
-// import react from 'react';
+import React, { useState } from 'react';
 
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
+const Hello = ({name, age}) => {
+    // const { name, age } = props
+
+    // const bornYear = () => {
+    //     const yearNow = new Date().getFullYear()
+    //     return yearNow - props.age
+    // }
+
+    const bornYear = () => new Date().getFullYear() - age
+
+    return (
+        <div>
+            <p>Hello {name}, you are {age} years old</p>
+            <p>So you were probably born on {bornYear()}</p>
+        </div>
+    )
 }
 
 
-const App = () => {
-  const name = 'Pekka'
-  const age = 10
+const App = (props) => {
+    const name = 'Pekka'
+    const age = 10
+    const {counter} = props
 
-  console.log('Hello from component')
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10}/>
-      <Hello name={name} age={age}/>
-      <Hello />
-    </div>
+    // console.log('Hello from component')
+    // return (
+    //     <div>
+    //         <h1>Greetings</h1>
+    //         <Hello name="Maya" age={26 + 10}/>
+    //         <Hello name={name} age={age}/>
+    //         <Hello />
+    //     </div>
+    // )
+    return (
+        <div>{counter}</div>
     )
-  }
-  
-  export default App;
+}
+
+export default App;
